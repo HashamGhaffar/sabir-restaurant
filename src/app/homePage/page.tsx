2e29118273682b8f4cd2ffcd56160a3d201ec61e
marsad <marsad11223@gmail.com>
2024-10-24 13:27:43 +0500

    removeing comments from home

--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -2,8 +2,6 @@ import { Box } from "@mui/material";
 import Hero from "./_sections/Hero";
 import Marquee from "./_sections/FoodItemMarquee";
 import OurMenu from "./_sections/OurMenu";
-// import SabirsGrillWelcome from "./_sections/SabirsGrillWelcome";
-import NewDishes from "./_sections/NewDishes";
 import Footer from "@/_components/footer/Footer";
 
 export default function HomePage() {
@@ -12,8 +10,6 @@ export default function HomePage() {
       <Hero />
       <Marquee />
       <OurMenu />
-      {/* <SabirsGrillWelcome /> */}
-      {/* <NewDishes /> */}
       <Footer />
     </Box>
   );
